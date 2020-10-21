013c94ecb446dd23c85cd49905053462d2b60f18
Anthony Lin <anthony.lin.198@gmail.com>
2020-10-21 11:48:13 -0700

    update config

---
+++ models/Problem.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const ProblemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please add a title"],
+    unique: true,
+    trim: true,
+    maxlength: [40, "Title cannot be more than 40 characters"],
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: [200, "Desscription cannot be more than 200 characters"],
+  },
+});
+
+module.exports =
+  mongoose.models.Problem || mongoose.model("Problem", ProblemSchema); // if the collection already exists
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+    MONGO_URI:
+      "mongodb+srv://anthonylin198:anthonylin198@igniteprogrammingdb.ucsf3.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  },
+};
