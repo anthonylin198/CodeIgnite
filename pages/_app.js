26c5c06ee6887473a4b43ed04118a3e466ed8451
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-21 09:59:22 -0700

    app update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "../styles/globals.css"; // Component is replaced here by any page
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
