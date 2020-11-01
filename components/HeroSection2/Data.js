dd772b3d40ecf9c0ab524c6bfd7c0af4be6abf4c
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-01 15:47:39 -0800

    newHero

---
+++ components/HeroSection2/Data.js
@@ -0,0 +1,18 @@
+export const splash = {
+  id: "fullstack",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Full Stack",
+  headline: "Build Full Stack Applications",
+  description:
+    "Learn programming fundamentals to developing online games and platforms",
+  buttonLabel: "Learn More",
+  // Change order of the image
+  imgStart: false,
+  img: require("../../public/images/react.svg"),
+  alt: "img",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
