28123713a6c2496158c148859274257a4dc3ada6
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-05 17:05:51 -0800

    fullstack course update

--- pages/fullstackcourse.js
+++ pages/fullstackcourse.js
@@ -3,7 +3,14 @@ import React from "react";
 const fullstackcourse = () => {
   return (
     <div>
-      <h1>We are here in the fullstack course</h1>
+      <h1>We are here in the fullstack course Title Section</h1>
+      <h2>Component with Course Progress</h2>
+      <h2>Section 0: Introduction with side scroll</h2>
+      <h2>Section 1: Javascript Fundamentals</h2>
+      <h2>Section 2: Web Development Fundamentals</h2>
+      <h2>Section 3: Frontend Guide - Popular Frameworks</h2>
+      <h2>Section 4: Server and Backend Guide</h2>
+      <h2>Section 5: Product Phase</h2>
     </div>
   );
 };
