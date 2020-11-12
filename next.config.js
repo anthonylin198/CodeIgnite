1ba8b6a600c62840fc0dba6fe5c3403584fab2f1
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-12 10:35:49 -0800

    assets updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 // next.config.js
 const withImages = require("next-images");
+const withFonts = require("next-fonts");
 
-module.exports = withImages({
-  env: {
-    MONGO_URI:
-      "mongodb+srv://anthonylin198:anthonylin198@igniteprogrammingdb.ucsf3.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  },
-});
+module.exports = withFonts(
+  withImages({
+    env: {
+      MONGO_URI:
+        "mongodb+srv://anthonylin198:anthonylin198@igniteprogrammingdb.ucsf3.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    },
+  })
+);
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,34 @@
-import "../styles/globals.css"; // Component is replaced here by any page
+// import "../styles/globals.css"; // Component is replaced here by any page
 import { Provider } from "react-redux";
 import store from "../redux/store";
+
+import Layout from "../components/Layout";
+import { GlobalProvider } from "../context/GlobalContext";
+
+import "../assets/fonts/fontawesome-5/webfonts/fa-brands-400.ttf";
+import "../assets/fonts/fontawesome-5/webfonts/fa-regular-400.ttf";
+import "../assets/fonts/fontawesome-5/webfonts/fa-solid-900.ttf";
+import "../assets/fonts/typography-font/Circular-Std-Book.ttf";
+import "../assets/fonts/typography-font/CircularStd-Black.ttf";
+import "../assets/fonts/typography-font/CircularStd-BlackItalic.ttf";
+import "../assets/fonts/typography-font/CircularStd-Bold.ttf";
+import "../assets/fonts/typography-font/CircularStd-BoldItalic.ttf";
+import "../assets/fonts/typography-font/CircularStd-Book.ttf";
+import "../assets/fonts/typography-font/CircularStd-BookItalic.ttf";
+import "../assets/fonts/typography-font/CircularStd-Medium.ttf";
+import "../assets/fonts/typography-font/CircularStd-MediumItalic.ttf";
+
+import "../assets/fonts/icon-font/fonts/avasta.ttf";
+import "../assets/fonts/icon-font/css/style.css";
+
+import "../components/Layout/bootstrap-custom.scss";
+import "../node_modules/slick-carousel/slick/slick.css";
+import "../node_modules/slick-carousel/slick/slick-theme.css";
+import "../node_modules/aos/dist/aos.css";
+
+import "../assets/fonts/icon-font/css/style.css";
+import "../assets/fonts/typography-font/typo.css";
+import "../assets/fonts/fontawesome-5/css/all.css";
 
 function MyApp({ Component, pageProps }) {
   return (
