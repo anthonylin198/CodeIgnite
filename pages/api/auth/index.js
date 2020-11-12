36825a9a1c1647c707ca19500fb1f3ffb15238e8
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-11 16:43:45 -0800

    login server complete

--- pages/api/auth/index.js
+++ pages/api/auth/index.js
@@ -31,6 +31,39 @@ export default async (req, res) => {
         res.status(400).json({ success: false, msg: "Token not valid" });
       }
       break;
+    case "POST":
+      const { email, password } = req.body; // destructuring out of the body
+      try {
+        // check if the user exists
+        let user = await User.findOne(email);
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "Invalid Credentials" }] });
+        }
+        // Match the email and the password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "Invalid Credentials" }] });
+        }
+
+        // Return the jsonwebtoken
+        const payload = {
+          user: {
+            id: user.id,
+          },
+        };
+        jwt.sign(payload, "secret", { expiresIn: 360000 }, (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        });
+        break;
+      } catch (err) {
+        res.status(500).send("Server error");
+      }
+    // take in the email password, check if that exists. If it does, then return the Jwt token
     default:
       res.status(400).json({ success: false });
       break;
