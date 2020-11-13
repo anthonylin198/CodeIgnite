e5aea40f6a0ad8bc09c146a1e074b762b8a0b2d2
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-13 15:50:00 -0800

    course clean up

--- pages/fullstackcourse.js
+++ pages/fullstackcourse.js
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 
-import Title from "../components/CoursePages/Title";
-import Progress from "../components/CoursePages/Progress";
 import Category from "../components/CoursePages/Category";
-import NavBar from "../components/LandingPages/Navbar";
 import HeroCenter from "../sections/common/HeroCenter";
-import ModuleCard from "../components/ModuleCard";
 import {
   overview,
   javascriptfundamentals,
@@ -23,12 +19,9 @@ const fullstackcourse = () => {
   };
   return (
     <div>
-      {/* <NavBar toggle={toggle} /> */}
       <HeroCenter title="Full-Stack Course">
         Modern Full Stack Web Development
       </HeroCenter>
-      {/* <Title /> */}
-      {/* <Progress /> */}
       <Category {...overview} />
       <Category {...javascriptfundamentals} />
       <Category {...webDevelopmentFundamentals} />
