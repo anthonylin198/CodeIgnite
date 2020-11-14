3cfa77b6f46e8551605dcfdd3a4e5f9ff3596e6e
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-14 13:27:40 -0800

    header update

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,13 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import styled from "styled-components";
 import { Container } from "react-bootstrap";
+import {
+  SiteHeader,
+  MobileLogin,
+  ToggleButton,
+  Menu,
+  MenuDropdown,
+} from "./HeaderElements";
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import Link from "next/link";
 
@@ -8,197 +15,22 @@ import GlobalContext from "../../context/GlobalContext";
 import Offcanvas from "../Offcanvas";
 import { Button } from "../Core";
 import NestedMenu from "../NestedMenu";
-import { device } from "../../utils";
 import Logo from "../Logo";
 import { menuItems } from "./menuItems";
 
-const SiteHeader = styled.header`
-  padding: 10px 0 10px 0;
-  position: absolute !important;
-  top: 0;
-  right: 0;
-  width: 100%;
-  z-index: 999;
-  @media ${device.lg} {
-    position: fixed !important;
-    transition: 0.4s;
-    &.scrolling {
-      transform: translateY(-100%);
-      transition: 0.4s;
-    }
-    &.reveal-header {
-      transform: translateY(0%);
-      box-shadow: 0 12px 34px -11px rgba(65, 62, 101, 0.1);
-      z-index: 9999;
-      background: ${({ dark, theme }) =>
-        dark ? theme.colors.dark : theme.colors.light};
-    }
-  }
-`;
-
-// create a div that will only popup on mobiel
-const MobileLogin = styled.a`
-  @media ${device.md} {
-    display: none;
-  }
-`;
-
-const ToggleButton = styled.button`
-  color: ${({ dark, theme }) =>
-    dark ? theme.colors.lightShade : theme.colors.darkShade}!important;
-  border-color: ${({ dark, theme }) =>
-    dark ? theme.colors.lightShade : theme.colors.darkShade}!important;
-`;
-
-const Menu = styled.ul`
-  @media ${device.lg} {
-    display: flex;
-    justify-content: flex-end;
-  }
-  .dropdown-toggle {
-    cursor: pointer;
-  }
-  > li {
-    > .nav-link {
-      @media ${device.lg} {
-        color: ${({ dark, theme }) =>
-          dark ? theme.colors.light : theme.colors.darkShade}!important;
-        font-size: 16px;
-        font-weight: 500;
-        line-height: 24px;
-        padding-top: 18px !important;
-        padding-bottom: 18px !important;
-        padding-left: 18px !important;
-        padding-right: 18px !important;
-      }
-      &:hover {
-        color: ${({ theme }) => theme.colors.primary} !important;
-      }
-    }
-  }
-  .nav-item.dropdown {
-    @media ${device.lg} {
-      position: relative;
-      z-index: 99;
-    }
-    &:hover {
-      > .menu-dropdown {
-        @media ${device.lg} {
-          top: 90%;
-          opacity: 1;
-          pointer-events: visible;
-        }
-      }
-    }
-  }
-`;
-
-const MenuDropdown = styled.ul`
-  list-style: none;
-  @media ${device.lg} {
-    top: 110%;
-    position: absolute;
-    min-width: 227px;
-    max-width: 227px;
-    box-shadow: 0 52px 54px rgba(65, 62, 101, 0.3);
-    border-radius: 8px;
-    border: 1px solid #e5e5e5;
-    background-color: #ffffff;
-    padding: 15px 0px;
-    z-index: 99;
-    opacity: 0;
-    transition: opacity 0.4s, top 0.4s;
-    pointer-events: none;
-    left: -90%;
-    border-radius: 0 0 10px 10px;
-    border: 1px solid #eae9f2;
-    background-color: #ffffff;
-    display: block;
-    border-top: ${({ theme }) => `3px solid ${theme.colors.secondary}`};
-  }
-  > .drop-menu-item {
-    color: ${({ theme }) => theme.colors.dark};
-    font-size: 16px;
-    font-weight: 300;
-    letter-spacing: -0.5px;
-    padding-left: 30px;
-    padding-right: 30px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-
-    a {
-      color: ${({ theme }) => theme.colors.dark};
-      transition: all 0.3s ease-out;
-      position: relative;
-      display: flex;
-      align-items: center;
-      &.dropdown-toggle::after {
-        display: inline-block;
-        vertical-align: 0.255em;
-        content: "";
-        border-top: 0.325em solid;
-        border-right: 0.325em solid transparent;
-        border-bottom: 0;
-        border-left: 0.325em solid transparent;
-        position: relative;
-        top: 1px;
-        margin-left: auto;
-        transform: rotate(-90deg);
-        transition: 0.4s;
-      }
-    }
-
-    &:hover {
-      > a {
-        color: ${({ theme }) => theme.colors.secondary};
-        text-decoration: none;
-        &::after {
-          transform: rotate(0deg);
-        }
-      }
-    }
-    &.dropdown {
-      position: relative;
-
-      &:hover {
-        > .menu-dropdown {
-          @media ${device.lg} {
-            top: 10px;
-            opacity: 1;
-            pointer-events: visible;
-            transform: translateX(-100%);
-          }
-        }
-      }
-      > .menu-dropdown {
-        border-top-color: ${({ theme }) => theme.colors.primary};
-        @media ${device.lg} {
-          top: 10px;
-          left: 0%;
-          opacity: 0;
-          transform: translateX(-110%);
-          transition: 0.4s;
-          pointer-events: none;
-        }
-        > .drop-menu-item {
-          @media ${device.lg} {
-            padding-left: 30px;
-            padding-right: 30px;
-          }
-        }
-      }
-    }
-  }
-  &.dropdown-right {
-    left: auto;
-    right: -90%;
-  }
-`;
+// redux
+import { useSelector, useDispatch } from "react-redux";
 
 const Header = ({ isDark = false }) => {
   const gContext = useContext(GlobalContext);
   const [showScrolling, setShowScrolling] = useState(false);
   const [showReveal, setShowReveal] = useState(false);
+  const check = useSelector((state) => state);
+
+  // todo: Useffect with loading the user data
+  useEffect(() => {
+    console.log("here", check);
+  }, []);
 
   useScrollPosition(({ prevPos, currPos }) => {
     if (currPos.y < 0) {
