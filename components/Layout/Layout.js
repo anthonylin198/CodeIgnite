43b769e43e9a2129aa0e41bcfb9b2af14f60fd2e
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-25 08:23:08 -0800

    layout fixup

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -36,6 +36,7 @@ const Loader = styled.div`
   z-index: 9999999999;
   opacity: 1;
   visibility: visible;
+  /* transition: all 0.2s ease-out 0.2s; */
   transition: all 0.2s ease-out 0.2s;
   &.inActive {
     opacity: 0;
